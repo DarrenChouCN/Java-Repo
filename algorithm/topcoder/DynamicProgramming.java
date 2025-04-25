949fdbeae78d8c149af0025b56303fc3518b65da
DarrenChou <darrenchoucn@gmail.com>
2025-04-25 11:29:22 +0930

    [4.25] GearsDiv2, HandsShaking

--- algorithm/topcoder/DynamicProgramming.java
+++ algorithm/topcoder/DynamicProgramming.java
@@ -115,6 +115,66 @@ public class DynamicProgramming {
   }
 
   /*
+   * GearsDiv2
+   * 
+   * Goose Tattarrattat has a machine that contains N gears (cogwheels). The gears
+   * are numbered 0 through N-1. Currently, the gears are arranged into a cycle:
+   * for each i, gear i meshes with gears i-1 and i+1 (counting modulo N). In
+   * particular, gear 0 meshes with gear N-1.
+   * 
+   * Let X and Y be two meshing gears. Note that if X is turning, Y must clearly
+   * be turning in the opposite direction (clockwise vs. counter-clockwise).
+   * 
+   * For each of the N gears we have a desired direction of turning. You are given
+   * this information encoded as a String Directions. Character i of Directions is
+   * 'R' if we want gear i to turn to the right (clockwise), and it is 'L' if we
+   * want it to turn to the left.
+   * 
+   * Of course, it may be impossible to satisfy all the desired directions of
+   * turning. Return the minimal number of gears that have to be removed from the
+   * machine so that all remaining gears can turn in the desired directions at the
+   * same time.
+   */
+  public int getmin(String directions) {
+    int len = directions.length();
+    if (len == 0)
+      return 0;
+    if (len == 1)
+      return 0;
+
+    int[][] dp1 = dpLinear(directions, 0, len - 2);
+    int[][] dp2 = dpLinear(directions, 1, len - 1);
+
+    int min1 = Math.min(dp1[len - 2][0], dp1[len - 2][1]) + 1;
+    int min2 = Math.min(dp2[len - 2][0], dp2[len - 2][1]) + 1;
+
+    return Math.min(min1, min2);
+  }
+
+  private int[][] dpLinear(String directions, int start, int end) {
+    int n = end - start + 1;
+
+    char first = directions.charAt(start);
+
+    // 0->L, 1->R
+    int[][] dp = new int[n][2];
+
+    dp[0][0] = first == 'L' ? 0 : 1;
+    dp[0][1] = first == 'R' ? 0 : 1;
+
+    for (int i = 1; i < n; i++) {
+      char curChar = directions.charAt(start + i);
+      int curDir = curChar == 'L' ? 0 : 1;
+      int curOpposite = 1 - curDir;
+
+      dp[i][curDir] = Math.min(dp[i - 1][curDir] + 1, dp[i - 1][curOpposite]);
+      dp[i][curOpposite] = dp[i - 1][curDir] + 1;
+    }
+
+    return dp;
+  }
+
+  /*
    * AvoidRoads
    * 
    * Problem contains images. Plugin users can view them in the applet.
@@ -226,11 +286,38 @@ public class DynamicProgramming {
     return dp[end[0]][end[1]][numMoves];
   }
 
+  /*
+   * HandsShaking
+   * Consider a meeting of n businessmen sitting around a circular table. To start
+   * the meeting, they must shake hands. Each businessman shakes the hand of
+   * exactly one other businessman. All handshakes happen simultaneously. We say
+   * that the shake is perfect if no arms cross each other. Given an int n, return
+   * the number of perfect shakes that exist for n businessmen. See examples for
+   * further clarification.
+   */
+  public long countPerfect(int n) {
+    if (n % 2 != 0)
+      return 0;
+
+    int pairs = n / 2;
+    long[] dp = new long[pairs + 1];
+
+    dp[0] = 1;
+
+    // a loop body similar to insertion sort
+    for (int i = 1; i <= pairs; i++) {
+      dp[i] = 0;
+      for (int k = 0; k < i; k++) {
+        dp[i] += dp[k] * dp[i - k - 1];
+      }
+    }
+
+    return dp[pairs];
+  }
+
   public static void main(String[] args) {
     DynamicProgramming dp = new DynamicProgramming();
 
-    int[] test1 = { 3, 7 };
-    int[] test2 = { 11, 5 };
-    System.out.println(dp.howMany(13, test1, test2, 14));
+    System.out.println(dp.getmin("RRLLRRRLRLLL"));
   }
 }
