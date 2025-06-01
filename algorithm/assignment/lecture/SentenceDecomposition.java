e452819dfda2a6ff5933d3c2df87fa9ba9f1c5cc
DarrenChou <darrenchoucn@gmail.com>
2025-06-01 21:28:24 +0930

    [6.1] SentenceDecomposition, ExpressAddOperators

---
+++ algorithm/assignment/lecture/SentenceDecomposition.java
@@ -0,0 +1,89 @@
+package lecture;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/*
+SentenceDecomposition
+
+  Little Bonnie and her friends were dismayed to learn that their parents were reading all of their private communications. They decided to invent a new language that would allow them to talk freely. What they finally came up with was a language where sentences are built using a special method.
+
+  All the valid words that can be used in the new language are given in the String[] validWords. A sentence is a concatenation (with no spaces) of a sequence of valid words. Each valid word can appear 0 or more times in the sentence. What makes the language special is that each word can be transformed by rearranging its letters before being used. The cost to transform a word is defined as the number of character positions where the original word and the transformed word differ. For example, "abc" can be transformed to "abc" with a cost of 0, to "acb", "cba" or "bac" with a cost of 2, and to "bca" or "cab" with a cost of 3.
+
+  Although several different sequences of valid words can produce the same sentence in this language, only the sequence with the least total transformation cost carries the meaning of the sentence. The advantage of the new language is that the parents can no longer understand what the kids are saying. The disadvantage is that the kids themselves also do not understand. They need your help.
+
+  Given a String sentence, return the total cost of transformation of the sequence of valid words which carries the meaning of the sentence, or -1 if no such sequence exists.
+ */
+public class SentenceDecomposition {
+
+  public int decompose(String sentence, String[] validWords) {
+    int n = sentence.length();
+    Map<String, Set<String>> graph = new HashMap<>();
+
+    for (String word : validWords) {
+      char[] arr = word.toCharArray();
+      Arrays.sort(arr);
+      String key = new String(arr);
+
+      if (!graph.containsKey(key)) {
+        graph.put(key, new HashSet<>());
+      }
+      graph.get(key).add(word);
+    }
+
+    Integer[] memo = new Integer[n + 1];
+
+    return dfs(0, sentence, graph, memo);
+  }
+
+  private int dfs(int index, String sentence, Map<String, Set<String>> graph, Integer[] memo) {
+    if (index == sentence.length())
+      return 0;
+    if (memo[index] != null)
+      return memo[index];
+
+    int minCost = Integer.MAX_VALUE;
+
+    for (int end = index + 1; end <= sentence.length(); end++) {
+      String sub = sentence.substring(index, end);
+      char[] chars = sub.toCharArray();
+      Arrays.sort(chars);
+      String key = new String(chars);
+
+      if (graph.containsKey(key)) {
+        for (String word : graph.get(key)) {
+          if (word.length() != sub.length())
+            continue;
+          int cost = calcCost(sub, word);
+          int next = dfs(end, sentence, graph, memo);
+          if (next != -1) {
+            minCost = Math.min(minCost, cost + next);
+          }
+        }
+      }
+    }
+
+    memo[index] = (minCost == Integer.MAX_VALUE) ? -1 : minCost;
+    return memo[index];
+  }
+
+  private int calcCost(String a, String b) {
+    int cost = 0;
+    for (int i = 0; i < a.length(); i++) {
+      if (a.charAt(i) != b.charAt(i))
+        cost++;
+    }
+    return cost;
+  }
+
+  public static void main(String[] args) {
+    SentenceDecomposition decomposition = new SentenceDecomposition();
+    String sentence = "ommwreehisymkiml";
+    String[] validWords = { "we", "were", "here", "my", "is", "mom", "here", "si", "milk", "where", "si" };
+    System.out.println(decomposition.decompose(sentence, validWords));
+  }
+
+}
--- algorithm/leetcode/class26/ExpressionAddOperators.java
+++ algorithm/leetcode/class26/ExpressionAddOperators.java
@@ -1,5 +1,6 @@
 package leetcode.class26;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /*
@@ -14,8 +15,58 @@ https://leetcode.com/problems/expression-add-operators/
   在一个数字字符串中插入 +、-、*，列出所有能算出目标值的表达式（不允许前导0）。
  */
 public class ExpressionAddOperators {
+
+  List<String> res = new ArrayList<>();
+  String num;
+  int target;
+
   public List<String> addOperators(String num, int target) {
-    return null;
+    this.num = num;
+    this.target = target;
+    // index, left, cur, expr
+    dfs(0, 0, 0, new StringBuilder(), true);
+    return res;
+  }
+
+  public void dfs(int index, long left, long cur, StringBuilder expression, boolean isFirst) {
+    if (index == num.length()) {
+      // 模拟运算优先级，
+      // left和cur为各自计算完成的部分，也就是乘法完成的部分；如果是全乘法，那么其中一个必为0
+      if (left + cur == target) {
+        res.add(expression.toString());
+      }
+      return;
+    }
+
+    long right = 0;
+    int len = expression.length();
+    for (int i = index; i < num.length(); i++) {
+      if (i > index && num.charAt(index) == '0')
+        break;
+      // 作为前缀推进一位
+      right = right * 10 + (num.charAt(i) - '0');
+
+      if (isFirst) {
+        expression.append(right);
+        dfs(i + 1, 0, right, expression, false);
+        // 通过setLength截取字符串，达到回溯dfs的效果
+        // 比如 "1+2" 尝试"*3"，失败后通过setLength直接可以回溯成"1+2"，有裁剪字符串的效果
+        expression.setLength(len);
+      } else {
+        expression.append('+').append(right);
+        dfs(i + 1, left + cur, right, expression, false);
+        expression.setLength(len);
+
+        expression.append('-').append(right);
+        dfs(i + 1, left + cur, -right, expression, false);
+        expression.setLength(len);
+
+        // 乘法在此处注意优先级
+        expression.append('*').append(right);
+        dfs(i + 1, left, cur * right, expression, false);
+        expression.setLength(len);
+      }
+    }
   }
 
 }
